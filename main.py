1cf44436d5d0d3794c79be944dff044bc5319c55
phantomf4321 <sajjadranjbaryazdi@gmail.com>
2023-11-19 16:43:52 +0330

    define variable

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ graph = [('A', 'B'), ('A', 'C'), ('A', 'D'), ('A', 'E')]
 
 # build concrete model with data specified
 model = ConcreteModel()
+
+
+# define variable
+model.x = Var( nodes, within=Binary )
