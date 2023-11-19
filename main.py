c1f862e74f712bb73ccec01601f827e3b0fad201
phantomf4321 <sajjadranjbaryazdi@gmail.com>
2023-11-19 16:43:06 +0330

    build concrete model with data specified

--- main.py
+++ main.py
@@ -1,19 +1,7 @@
 from pyomo.environ import *
 
-sample_graph = [(0,1), (0,5), (0,4), (0,3)]
-weight = [1, 1, 1, 1]
+nodes = ['A', 'B', 'C', 'D', 'E']
+graph = [('A', 'B'), ('A', 'C'), ('A', 'D'), ('A', 'E')]
 
-"""
-
-M.ITEMS = Set(initialize=value.keys())
-M.x = Var(M.ITEMS, domain=Binary)
-
-M.value = Objective(expr=sum(value[i]*M.x[i] for i in M.ITEMS),sense=maximize)
-M.weight = Constraint(expr=sum(weight[i]*M.x[i] for i in M.ITEMS) <=Capacity)
-
-solver = SolverFactory('glpk', executable='/usr/bin/glpsol')
-solver.solve(M)
-
-for i in value.keys():
-    print('  ', i, ':', M.x[i]())
-print("objective:",M.value())"""
+# build concrete model with data specified
+model = ConcreteModel()
