611f412c16337cbf2a0adde21ecf4f249979ad90
phantomf4321 <sajjadranjbaryazdi@gmail.com>
2023-11-19 16:55:59 +0330

    there is a problem in define constraint

--- main.py
+++ main.py
@@ -12,3 +12,6 @@ model.x = Var( nodes, within=Binary )
 
 # define constraint
 model.value = Objective(expr = sum(model.x[i] for i in nodes), sense=minimize)
+
+# define constraint
+model.weight = Constraint(expr = (model.x[i[0]] + model.x[i[1]] for i in graph) >= 1 )
