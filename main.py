08b0bbe07b6c3864e10b333fe7de479be25bfb4c
phantomf4321 <sajjadranjbaryazdi@gmail.com>
2023-11-19 16:32:07 +0330

    create simple graph

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from pyomo.environ import *
 
-weight = {'Bag1':300, 'Bag2':1, 'Bag3':200, 'Bag4':100, 'Bag5':10, 'Bag6':54}
-value = {'Bag1':4000, 'Bag2':5000, 'Bag3':5000, 'Bag4':2000, 'Bag5':1000, 'Bag6':2834}
+sample_graph = [(0,1), (0,5), (0,4), (0,3)]
+weight = [1, 1, 1, 1]
 
-Capacity = 106
-M = ConcreteModel()
+"""
 
 M.ITEMS = Set(initialize=value.keys())
 M.x = Var(M.ITEMS, domain=Binary)
@@ -17,4 +16,4 @@ solver.solve(M)
 
 for i in value.keys():
     print('  ', i, ':', M.x[i]())
-print("objective:",M.value())
+print("objective:",M.value())"""
