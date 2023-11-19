33f80c105bb3a42f61cd087ee8f64e0bd28c9feb
phantomf4321 <sajjadranjbaryazdi@gmail.com>
2023-11-19 11:37:40 +0330

    test sample data

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-import pyomo
+from pyomo.environ import *
+
+weight = {'Bag1':300, 'Bag2':1, 'Bag3':200, 'Bag4':100, 'Bag5':10, 'Bag6':54}
+value = {'Bag1':4000, 'Bag2':5000, 'Bag3':5000, 'Bag4':2000, 'Bag5':1000, 'Bag6':2834}
+
+Capacity = 106
+M = ConcreteModel()
+
+M.ITEMS = Set(initialize=value.keys())
+
+M.x = Var(M.ITEMS, domain=Binary)
