e9443e0c306c3017e2b27e1999c530f229f4b296
phantomf4321 <sajjadranjbaryazdi@gmail.com>
2023-11-21 16:38:22 +0330

    test new graph

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from pyomo.environ import *
 
 nodes = ['A', 'B', 'C', 'D', 'E']
-graph = [('A', 'B'), ('A', 'C'), ('A', 'D'), ('A', 'E')]
+graph = [('A', 'C'), ('B', 'C'), ('B', 'D'), ('D', 'E')]
 
 # build concrete model with data specified
 model = ConcreteModel()
