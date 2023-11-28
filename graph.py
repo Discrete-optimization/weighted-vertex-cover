b84963081703be68d4a20cc0354783513c822e50
phantomf4321 <sajjadranjbaryazdi@gmail.com>
2023-11-28 16:53:11 +0330

    get_random_string

--- graph.py
+++ graph.py
@@ -2,6 +2,14 @@ import matplotlib.pyplot as plt
 from itertools import combinations, groupby
 import networkx as nx
 import random
+import string
+
+
+def get_random_string(length):
+    # choose from all lowercase letter
+    letters = string.ascii_lowercase
+    result_str = ''.join(random.choice(letters) for i in range(length))
+    return(result_str)
 
 def gnp_random_connected_graph(n, p):
     """
@@ -27,17 +35,13 @@ def gnp_random_connected_graph(n, p):
 
 probability = 0.1
 G = nx.Graph()
-
 #G = nx.erdos_renyi_graph(100, 0.1)
-G = nx.fast_gnp_random_graph(100, 0.1, seed=None, directed=False)
+G = nx.fast_gnp_random_graph(55, 0.1, seed=None, directed=False)
 #G = gnp_random_connected_graph(100,0.01)
 
-node_list = G.nodes
-
-print(type(node_list))
-print(G.edges)
+print(get_random_string(3))
 color_map = []
-for node in node_list:
+for node in G.nodes:
     if node < 50:
         color_map.append('blue')
     else:
@@ -48,4 +52,4 @@ nx.draw(G, node_color=color_map,
         with_labels=True,
         node_size=500)
 
-plt.show()
+#plt.show()
