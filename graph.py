d53230ab6dc53d3aed47c96aa113542d27105473
phantomf4321 <sajjadranjbaryazdi@gmail.com>
2023-11-29 12:41:35 +0330

    class graph

--- graph.py
+++ graph.py
@@ -4,83 +4,56 @@ import networkx as nx
 import random
 import string
 
+class Graph:
+    def __init__(self, nodes, edges):
+        print("Graph constructor is called!")
+        self.nodes = nodes
+        self.edge = edges
 
+    def get_nodes(self):
+        return self.nodes
+    def get_edeges(self):
+        return self.edge
 
-def get_random_lable(length):
-    # choose from all lowercase letter
-    letters = string.ascii_lowercase
-    result_str = ''.join(random.choice(letters) for i in range(length))
-    return(result_str)
+    def get_random_lable(sef, length):
+        # choose from all lowercase letter
+        letters = string.ascii_lowercase
+        result_str = ''.join(random.choice(letters) for i in range(length))
+        return (result_str)
 
-def node_genarator(length):
-    curr_edge = []
-    while(len(curr_edge) < length):
-        node = get_random_lable(3)
-        if node not in curr_edge:
-            curr_edge.append(node)
+    def node_genarator(self, length):
+        curr_node = []
+        while (len(curr_node) < length):
+            node = self.get_random_lable(3)
+            if node not in curr_node:
+                curr_node.append(node)
 
-    return curr_edge
+        self.nodes = curr_node
 
-def edge_genarator(nodes, length):
-    edge = []
-    for counter in range (0, length):
-        curr_edge = [random.choice(nodes), random.choice(nodes)]
-        edge.append(curr_edge)
+    def edge_genarator(self, length):
+        edge = []
+        for counter in range(0, length):
+            curr_edge = [random.choice(self.nodes), random.choice(self.nodes)]
+            edge.append(curr_edge)
 
-    return edge
+        self.edge = edge
 
-def gnp_random_connected_graph(n, p):
-    """
-    Generates a random undirected graph, similarly to an Erdős-Rényi
-    graph, but enforcing that the resulting graph is conneted
-    """
-    edges = combinations(range(n), 2)
-    G = nx.Graph()
-    G.add_node(range(n), weight=2)
-    if p <= 0:
+    def construct(self):
+        G = nx.Graph()
+        G.add_edges_from(self.edge)
         return G
-    if p >= 1:
-        return nx.complete_graph(n, create_using=G)
-    for _, node_edges in groupby(edges, key=lambda x: x[0]):
-        node_edges = list(node_edges)
-        random_edge = random.choice(node_edges)
-        G.add_edge(*random_edge)
-        for e in node_edges:
-            if random.random() < p:
-                G.add_edge(*e)
-    return G
+
+    def plot(self, G):
+        plt.figure(figsize=(16, 10))
+        nx.draw(G, node_color='blue',
+                with_labels=True,
+                node_size=500,
+                font_color='white'
+                )
+
+        plt.show()
 
 
-number_of_nodes = 15
-number_of_edges = 45
-G = nx.Graph()
-nodes = node_genarator(number_of_nodes)
-print(nodes)
-edges = edge_genarator(nodes, number_of_edges)
-print(edges)
 
 
-#G = nx.erdos_renyi_graph(100, 0.1)
-#G = nx.fast_gnp_random_graph(55, 0.1, seed=None, directed=False)
-#G = gnp_random_connected_graph(100,0.01)
-G.add_edges_from(edges)
 
-"""curr_lable = []
-for ed in edges:
-    l = {(ed[0], ed[1]): '1'}
-    curr_lable.append(l)
-
-print(curr_lable)"""
-
-plt.figure(figsize=(16,10))
-nx.draw(G, node_color='blue',
-        with_labels=True,
-        node_size=500,
-        font_color='white'
-        )
-"""
-edge_labels={('A', 'B'): 'AB',
-             ('B', 'C'): 'BC',
-             ('B', 'D'): 'BD'},
-"""
-plt.show()
