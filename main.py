68b5b99f20685fb1640475dc3a3d1924f5f30930
phantomf4321 <sajjadranjbaryazdi@gmail.com>
2023-11-29 13:16:42 +0330

    there is problem on color map

--- main.py
+++ main.py
@@ -9,25 +9,19 @@ G1 = Graph(number_of_nodes, number_of_edges)
 
 G1.node_genarator()
 G1.edge_genarator()
-
 nodes = G1.get_nodes()
 graph = G1.get_edeges()
 
 # build concrete model with data specified
 model = ConcreteModel()
-
-
 # define variable
 model.x = Var( nodes, within=Binary)
-
 # define constraint
 model.value = Objective(expr = sum(model.x[i] for i in nodes), sense=minimize)
-
 # define ConstraintList
 model.constraints = ConstraintList()
 for (i, j) in graph:
     model.constraints.add(model.x[i] + model.x[j] >= 1)
-
 # solving model
 solver = SolverFactory('glpk')
 solver.solve(model)
@@ -37,7 +31,7 @@ color_map = []
 for i in nodes:
     total = sum(model.x[j]() for j in nodes)
     print(i, model.x[i]())
-    if(model.x[i]() == 1):
+    if(model.x[i]() > 0):
         color_map.append("red")
     else:
         color_map.append("blue")
