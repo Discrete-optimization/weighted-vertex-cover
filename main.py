53d6b782fc7cb14819c9a2960b06ad814125aeaf
phantomf4321 <sajjadranjbaryazdi@gmail.com>
2023-12-02 19:10:17 +0330

    Make a simple graph

--- main.py
+++ main.py
@@ -2,22 +2,17 @@ from model import Model
 from graph import Graph
 
 
-#Enter the number of nodes and edges you want:
-number_of_nodes = 100
-number_of_edges = 150
+#Make a simple graph:
+nodes = ["A", "B", "C", "D", "E", "F"]
+weigth = [1, 5, 3, 9, 2, 4]
+graph = [("A", "B"), ("A", "C"), ("A", "D"), ("A", "E"), ("A", "F")]
 
 #construct a graph:
-G1 = Graph(number_of_nodes, number_of_edges)
+G1 = Graph(len(nodes), len(graph))
 
-G1.node_genarator()#generate random nodes ["A", "B", "C", "D"]
-G1.weigth_genarator()#generate random weigth between 1 to 10 [1, 5, 3, 9]
-G1.edge_genarator()#denerate random edge between ndes [("A", "B"), ("A", "C"), ("A", "D")]
-G1.remove_tube()#using this function is optional. Removes tubes. [("A", "B"), ("C", "C")] -> [("A", "B")]
-
-#set nodes and graph with nodes an edges:
-nodes = G1.get_nodes()
-weigth = G1.get_weigth()
-graph = G1.get_edeges()
+G1.set_special_nodes(nodes)
+G1.set_special_weigth(weigth)
+G1.set_special_graph(graph)
 
 #call pyomo create and solve modle:
 m1 = Model(nodes, graph, weigth)
